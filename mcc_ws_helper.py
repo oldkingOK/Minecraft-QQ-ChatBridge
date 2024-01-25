0fa9344ed63f4f07101b5075148678c512b74adb
oldkingOK <oldkingok@qq.com>
2024-01-25 12:31:42 +0800

    🐞 fix(mcc_ws_helper.py): 修复无法正常检测“死亡信息”的bug
    通过mcc语言文件en_us.json，导出所有以`death.`开头的键值，然后作为判断死亡消息的依据

--- mcc_ws_helper.py
+++ mcc_ws_helper.py
@@ -1,8 +1,14 @@
 from websockets.sync.client import connect
+from text_util import remove_color_char
+from typing import Callable
 import json
 import re
 
 websocket = None
+DEBUG_MODE = True
+DEATH_MESSAGE_FILE = "./asset/death_msg.json"
+DEATH_MESSAGE_PATTERNS = {}
+"""存放死亡消息的正则表达式，用于判断死亡消息"""
 
 def connect_and_auth(address: str, password: str):
     """
@@ -19,22 +25,30 @@ def connect_and_auth(address: str, password: str):
     }
     websocket.send(json.dumps(data))
     message = websocket.recv()
-    print(f"Received: {message}")
+    if DEBUG_MODE: print(f"Received: {message}")
 
-def start_recv():
+def start_recv(msg_handler: Callable[[str], None]):
     global websocket
     try:
         while True:
             message = websocket.recv()
-            handle_mcc_json(mcc_json=json.loads(message))
+            handle_result = handle_mcc_json(mcc_json=json.loads(message))
+            msg_handler(handle_result)
     
     except KeyboardInterrupt:
         print("Exit")
 
-def handle_mcc_json(mcc_json):
+def handle_mcc_json(mcc_json) -> str | None:
+    """
+    处理mcc发进来的消息
+    如果返回None，就是其他消息
+    如果返回Str，就是要发到QQ的消息
+    """
+    result = None
+    
     match mcc_json["event"]:
         case "OnTimeUpdate" | "OnLatencyUpdate" | "OnServerTpsUpdate":
-            return
+            result = None
         case "OnChatRaw":
             """
             "text":"<oldkingOK> hi"
@@ -46,26 +60,66 @@ def handle_mcc_json(mcc_json):
                 包含 "has made the advancement"
             """
             text = json.loads(mcc_json["data"])["text"]
-            print(f"The text is {text}")
 
-            pattern_player_chat = r'<[a-zA-Z]*> .*'
-            pattern_death = r'.*\[death\..*'
+            pattern_player_chat = r'^<([a-zA-Z0-9_]+)> (.+)$'
+            """判断玩家聊天的正则表达式"""
             pattern_achievement = r'.*has made the advancement.*'
+            """判断成就信息的正则表达式"""
             if re.fullmatch(pattern_player_chat, text):
-                print(text)
+                result = text
             elif re.fullmatch(pattern_achievement, text):
                 # ok_bot has made the advancement §a[§aDiamonds!]
-                # TODO 需要删掉颜色符号
-                print(f"<喜报> {text}")
-            elif re.fullmatch(pattern_death, text):
-                # 这里需要找到对应的翻译文件，比如从server-1.20.1.jar解包出的en_US.json：
-                # "death.attack.genericKill.player": "%1$s was killed whilst fighting %2$s"
-                # 否则就会像下面这样显示：
-                # [death.attack.genericKill.player] oldkingOK Zombie
-                print(f"<悲报> {text}")
+                result = f"<喜报> {remove_color_char(text)}"
+            elif is_death_msg(text):
+                """
+                大多数死亡原因能够正常显示，
+                由于mcc/MinecraftClient/Resources/en_us.json中未包含对应的翻译（未更新）
+                导致显示
+                [death.attack.genericKill.player] oldkingOK Zombie
+                TODO 向mcc提交pull request
+                """
+                result = f"<悲报> {text}"
             else:
-                print("未知消息")
+                result = text
 
-            print(json.dumps(mcc_json))
         case "OnDisconnect" | _:
-            print(json.dumps(mcc_json))
+            if DEBUG_MODE: print(f"The origin mcc_json is: {json.dumps(mcc_json)}")
+            return None
+    
+    if DEBUG_MODE: 
+        print(f"The handle_mcc_json result is: {result}")
+        print(f"The origin mcc_json is: {json.dumps(mcc_json)}")
+
+    return result
+
+def build_regex_pattern(pattern_str):
+    # 将 %1$s, %2$s, %3$s 替换为玩家名
+    pattern_str = re.sub(r'%([1-3]\$s)', r'([a-zA-Z0-9_]+)', pattern_str)
+    # 构建正则表达式模式
+    return re.compile(pattern_str)
+
+def load_death_msg():
+    death_msg_dict = json.load(open(DEATH_MESSAGE_FILE, 'r'))
+    global DEATH_MESSAGE_PATTERNS
+    DEATH_MESSAGE_PATTERNS = [build_regex_pattern(msg) for msg in list(death_msg_dict.values())]
+
+def is_death_msg(msg) -> bool:
+    """判断是否是死亡消息"""
+    global DEATH_MESSAGE_PATTERNS
+    if len(DEATH_MESSAGE_PATTERNS) == 0: load_death_msg()
+        
+    for pattern in DEATH_MESSAGE_PATTERNS:
+        if pattern.match(msg):
+            # 提取匹配的捕获组值
+            return True
+        
+    return False
+
+# "oldkingOK was killed whilst fighting Fish"
+def test_death():
+    print(handle_mcc_json(
+        {
+            "event":"OnChatRaw",
+            "data":"{\"text\":\"oldkingOK was killed whilst fighting Fish\"}"
+        }
+    ))
