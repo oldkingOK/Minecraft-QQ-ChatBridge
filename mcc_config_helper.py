a52bb09fec970d1dabe78338ce3f26b7993d0371
oldkingOK <oldkingok@qq.com>
2024-01-25 10:46:36 +0800

    🐞 fix(mcc_config_helper): 修复创建路径会崩溃的bug

--- mcc_config_helper.py
+++ mcc_config_helper.py
@@ -1,6 +1,6 @@
 import json
 import toml
-from os import mkdir
+from os import mkdir, path
 from dict_util import replace_values_recursive
 
 def load_config(config_json_name: str, mcc_ini_template: str, tmp_folder: str):
@@ -27,10 +27,21 @@ def load_config(config_json_name: str, mcc_ini_template: str, tmp_folder: str):
         replace_values_recursive(mcc_config_template_json, config_json["Servers"][server_name]["Config"])
 
         # 输出
-        
-        mkdir(tmp_folder)
+        if not path.exists(tmp_folder): mkdir(tmp_folder)
         with open(f"{tmp_folder}/{server_name}.ini", "w") as output_file:
             toml.dump(mcc_config_template_json, output_file)
+
+def get_server_dict(config_json_name: str) -> dict:
+    with open(config_json_name, 'r') as config_json_file:
+        config_json = json.load(config_json_file)
+
+    result = {}
+    servers = config_json["Servers"]
+    for server_name in servers:
+        server = servers[server_name]
+        result[server_name] = server["Enabled"]
+
+    return result
 
 # def main():
 #     load_config("config.json", "mcc_config_template.ini", "./tmp")
